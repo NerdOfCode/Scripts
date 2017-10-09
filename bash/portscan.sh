5790c22314127d993605a3e927aed527c50184a4
NerdOfCode <NerdOfCode@users.noreply.github.com>
2017-10-09 12:15:26 -0500

    Create portscan.sh
    Initial upload

---
+++ bash/portscan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+timeout_time=.5
+
+
+read -p "Enter hostname: " hostname
+read -p "Enter port/ports range(Ex: 1-443 or 443): " ports
+high=$(echo $ports| cut -f2 -d"-")
+low=$(echo $ports | cut -f1 -d"-")
+
+est_time=$(echo "$timeout_time * $high" | bc)
+
+echo "Max Estimated Time: $est_time seconds"
+for (( i=$low; i <= $high; i++)){
+timeout $timeout_time nc -z -v $hostname $i >/dev/null
+if [ $? == 0 ]
+then
+echo "$i is open!"
+fi
+}
+exit 0
