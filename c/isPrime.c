82f35922a1b9ff845df51b9aade938f8c8e81cfd
NerdOfCode <NerdOfCode@users.noreply.github.com>
2017-10-21 23:50:35 -0500

    Initial Upload

---
+++ c/isPrime.c
@@ -0,0 +1,30 @@
+//Author: NerdOfCode
+//Notes: This program or script whatever is not designed to handle big numbers
+//treat it with respect
+
+
+#include <stdio.h>
+
+int main(){
+        unsigned int _numb_;
+        unsigned int start = 2;
+        unsigned int get_numb_half;
+
+
+        printf("Enter number to test: ");
+
+        scanf("%d", &_numb_);
+        get_numb_half = _numb_ / 2;
+        while(get_numb_half > start){
+                if(_numb_ % start == 0){
+                        printf("%d is prime\n", _numb_);
+                        return 0;
+                }else{
+                        start++;
+                        printf("%d",start);
+
+                }
+        }
+        return 0;
+
+}
