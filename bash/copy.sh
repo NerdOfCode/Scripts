909676b68626bf4b0ee343134c141092ab365d50
NerdOfCode <NerdOfCode@users.noreply.github.com>
2017-10-24 17:35:31 -0500

    Update copy.sh
    Increased redability

--- bash/copy.sh
+++ bash/copy.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+#Author: NerdOfCode
+#Purpose: Copy files or directories
+
+
 
 if [ $(whoami) != "root" ]
 then
-echo "This script may not work as expected without sudo powers"
-sleep 3
+  echo "This script may not work as expected without sudo powers"
+  sleep 3
 fi
+
 read -p "Enter path to copy the files/folders: " path1
 read -p "Enter path for the files/folders to be saved: " path
+
 if [ -d $path ]
 then
-echo -e "Copying files/folders: \n"
-cp -r -v $path1/* $path
-exit
+  echo -e "Copying files/folders: \n"
+  cp -r -v $path1/* $path
+  exit 0
+  
 else
-mkdir $path
-echo -e "Copying files/folders: \n"
-cp -r -v $path1/* $path
-exit
+  mkdir $path
+  echo -e "Copying files/folders: \n"
+  cp -r -v $path1/* $path
+  exit 0
 fi
