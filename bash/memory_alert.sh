ec5763caca64d9d63b3983ac3c4639de148c663b
NerdOfCode <admin@nerdofcode.com>
2017-12-06 21:25:43 -0800

    Create memory_alert.sh

---
+++ bash/memory_alert.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+##########################
+#Disclaimer:
+#Memory Alert is currently a work in progress and is not production ready...
+###########################
+
+#Change values below
+
+email="admin@nerdofcode.com"
+low_memory="100" #In KB
+
+
+#############################
+
+#Requirement checking below
+
+if [ -z /proc/meminfo ]
+then
+        echo "Sorry but your system is not supported..."
+        exit 1
+fi
+
+
+if [ $(whoami) != root ]
+then
+        echo "This script needs to be run as root"
+        exit 1
+fi
+
+if [ -z /var/log/memory_alert.log ]
+then
+        echo "Welcome to memory alert!!!"
+        touch /var/log/memory_alert.log
+fi
+
+################################
+
+#Magic happens below
+
+free_mem=$(sudo cat /proc/meminfo | grep MemFree: | tr 'MemFree:' ' '  | tr 'kB' ' '  | sed 's/ //g' )
+if [ $free_mem -lt $low_memory  ]
+then
+        echo "Low memory!!! Free memory $free_memkB" | mail -s "Low Memory" $email
+fi
+
+
+#Reset timer
+sudo -k
+
+exit 0
