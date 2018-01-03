82d426bcca3dd135dcde53e79aa552f4ee0e0511
NerdOfCode <admin@nerdofcode.com>
2018-01-02 23:28:54 -0600

    Enhanced isPrime.cpp
    Improved the overall look of the isPrime.cpp file

--- CPP/isPrime.cpp
+++ CPP/isPrime.cpp
@@ -4,20 +4,32 @@
 using namespace std;
 
 int main(){
+  
   int isPrime;
   int guess=2;
+  
   cout << "Enter number to test if it is prime: ";
   cin >> isPrime;
+  
   while(isPrime >= guess){
+    
     if( guess != isPrime && isPrime%guess == 0 ){
+      
       cout << isPrime << " is not a prime number!" << endl;
       break;
+      
     }else if(guess==isPrime){
+      
       cout << isPrime << " is a prime number!"<< endl;
       break;
+      
     }else{
+      
       guess++;
-}
+
+    }
   }
+  
 return 0;
+  
 }
