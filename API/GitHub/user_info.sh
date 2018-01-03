d773d47a069b61830834273058fd0461d911173b
NerdOfCode <admin@nerdofcode.com>
2018-01-03 16:51:07 -0600

    Created a new API script

---
+++ API/GitHub/user_info.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Cache directory
+dir=".github_cache"
+
+if [ ! -d ~/$dir ]
+then
+	mkdir ~/$dir
+	echo "Cache Enabled"
+	echo "To use the cache, place at least one arguement..."
+	echo "Ex: ./github_api.sh cache"
+fi
+
+read -p "Enter Github Username: " user
+
+if [ ! -f ~/$dir/$user.txt ]
+then
+	echo "User not cached"
+	shift
+fi
+
+
+if [ $# != 1 ]
+then
+	curl --silent https://api.github.com/users/$user > ~/$dir/$user.txt
+else
+	echo "Using cache"
+fi
+
+
+#Shortcut
+file=$(cat ~/$dir/$user.txt)
+
+id=$(echo $file | jq '.id')
+type=$(echo $file | jq '.type')
+email=$(echo $file | jq '.email')
+followers=$(echo $file | jq '.followers')
+following=$(echo $file | jq '.following')
+bio=$(echo $file | jq '.bio')
+public_repos=$(echo $file | jq '.public_repos')
+
+echo "Id: $id"
+echo "Type: $type"
+
+if [ $email != "null" ]
+then
+	echo "Email: $email"
+fi
+
+echo "Followers: $followers"
+echo "Following: $following"
+echo "Public Repos: $public_repos"
+echo "Bio: $bio"
