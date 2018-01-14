79037559232d1255ffa3a14e98ce9aa69e3a7c87
NerdOfCode <admin@nerdofcode.com>
2018-01-14 12:20:02 -0600

    Improved support

--- GUI/arch_gui_upgrade.py
+++ GUI/arch_gui_upgrade.py
@@ -3,12 +3,28 @@
 from tkinter import *
 import tkinter, os, sys
 
+#Try to support python2 and 3
+try:
+	from Tkinter import *
+	import Tkinter, os, sys
+
+except ImportError:
+	from tkinter import *
+	import tkinter, os, sys
+
 print("Warning: This script needs sudo access as it upates/upgrades your system via pacman")
 
 def runCheck():
 	
 	os.system("sudo pacman -Syu")
 	print("Updating")
+	root.destroy()
+	sys.exit(1)
+
+
+#Obviously used for exiting GUI and script!
+def exit():
+	root.destroy()
 	sys.exit(1)
 
 
@@ -17,13 +33,17 @@ update = 0
 #Set root
 root = tkinter.Tk()
 
+#Window size
+root.minsize(300,150)
+#root.maxsize(750,750)
 
 #set title
 root.title("Arch-Linux Update/Upgrade")
 
-submit = tkinter.Button(root, text = "Update", width = 50, height = 5, command = 
-runCheck)
+submit = tkinter.Button(root, text = "Update", command = runCheck)
+submit.place(relx=.4, rely=.4)
 
-submit.pack()
+cancel = tkinter.Button(root, text = "Cancel ", command = exit)
+cancel.place(relx=.4, rely=.6)
 
 root.mainloop()
