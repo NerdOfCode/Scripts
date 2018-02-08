d565066921b19fde7581b97bed10ee47e77b1260
NerdOfCode <admin@nerdofcode.com>
2018-02-08 15:39:11 -0600

    Improving script capabilities

--- Bash/bash_shortcuts.sh
+++ Bash/bash_shortcuts.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Author: NerdOfCode
 #Purpose: Adds a few alias's that improve the efficieny of your terminal
+
 #Tested: Linux Mint 18.1 Serena | Kernel Version 4.4.0-79-generic
+#Tested: Antergos | Kernel Version: 4.15.1
 
 #Storage for alias's
 storage='.1bashrc'
@@ -16,10 +18,23 @@ done
 
 }
 
+function custom(){
+	printf "Enter custom alias: "
+	read alias
+
+	echo -e "Adding:\n"
+	echo -e "$alias\n" | tee -a ~/$storage
+	sleep 2
+	echo "NOTE: you may need to manually run source ~/$storage..."
+	source ~/$storage
+	exit 0
+
+}
 
 function add_shortcuts(){
 	if [ -f ~/$storage ]
 	then
+		echo "Shortcuts already exist..."
 		echo "Please delete $storage and try again"
 		exit 1
 	fi
@@ -36,6 +51,7 @@ function add_shortcuts(){
 	echo -e "\nNOTE: You may need to manually run 'source $storage'"
 
   	exit 0;}
+
 function remove_shortcuts(){
 
 	if [ -f ~/$storage ]
@@ -44,19 +60,22 @@ function remove_shortcuts(){
 		rm ~/$storage
   		exit 0
 	fi
-	echo "The shortcuts arent installed"
+	echo "The shortcuts aren't installed"
 	exit 1;}
+
 function options(){
 	case $response in
         	"1") 
                 	add_shortcuts;;
         	"2")
                 	remove_shortcuts;;
-        	"4")
+        	"5")
                 	echo "Good bye";exit;;
 		"3")
 			available_commands;;
-        	*)
+        	"4")
+			custom;;
+		*)
                 	echo "Unknown option selected";;
 	esac
 }
@@ -65,7 +84,7 @@ function options(){
 clear
 while :
 do
-	echo -e "1)Add the shortcuts \n2)Remove the shortcuts\n3)Show available alias's\n4)Exit"
+	echo -e "1)Add the shortcuts \n2)Remove the shortcuts\n3)Show available alias's\n4)Add Custom Alias\n5)Exit"
 	read -p "Response: " response
 	options
 	sleep 3
