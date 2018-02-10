eb2ae09eb671654e9f5429df61003dc5490154f8
NerdOfCode <admin@nerdofcode.com>
2018-02-10 14:20:06 -0600

    Added sudo checker

---
+++ Bash/functions/is_root.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Purpose: Tell if the user is running script via root or not
+#If script exits with exit status of 2 then no root
+#If zero then root
+
+
+if [ "$EUID" -ne 0 ]
+then
+	exit 2
+fi
+
+exit 0 
