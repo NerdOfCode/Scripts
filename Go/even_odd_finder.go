0db490ac9a75ad2a4c1d54319398ccaf7589a420
NerdOfCode <admin@nerdofcode.com>
2018-02-16 20:14:50 -0600

    Added new file for odd/even

---
+++ Go/even_odd_finder.go
@@ -0,0 +1,56 @@
+/*
+	Purpose: Finds even or odd numbers depending on Input
+	Note: Uses common logic to find even/odd values
+	Updated: 2/16/18
+*/
+
+package main
+
+import (
+	"fmt"
+	)
+
+func even() {
+	var limit int
+
+	fmt.Print("Enter maximum limit: ")
+	fmt.Scanln(&limit)
+
+	for start_c := 1; limit > start_c; start_c++{
+		start_c++
+		fmt.Println(start_c)
+	}
+}
+
+func odd(){
+	var limit int
+
+	fmt.Print("Enter maximum limit: ")
+	fmt.Scanln(&limit)
+
+	for start_c := 0; limit >= start_c; start_c++{
+		start_c++
+		fmt.Println(start_c)
+	}
+
+
+}
+
+
+func main(){
+
+	var decision string
+
+	fmt.Print("Would you like to find (e)ven or (o)dd numbers: ")
+	fmt.Scanln(&decision)
+
+	switch decision{
+		case "e":
+			even()
+		case "o":
+			odd()
+
+	}
+
+
+}
