185e4c60690890ac661d61baf714bc731524e284
NerdOfCode <admin@nerdofcode.com>
2018-02-19 13:10:41 -0600

    Added script to get public ip

---
+++ Bash/get_public_ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+which dig >/dev/null 2>&1
+
+if [ $? != 0 ]
+then
+	echo "Please install dig command..."
+	exit 1
+fi
+
+ip=$(dig +short myip.opendns.com)
+
+echo "Your public IP is: $ip"
