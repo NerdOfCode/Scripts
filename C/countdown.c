a99a8c8131ce629f974dd157fbe9217c01db1d4c
NerdOfCode <admin@nerdofcode.com>
2018-03-02 22:40:39 -0600

    Improved CLI args

--- C/countdown.c
+++ C/countdown.c
@@ -1,6 +1,6 @@
 /*
 
-Purpose: A simple count down timer according to user input
+Purpose: A super simple count down timer according to user input
 Author: NerdOfCode
 Updated: 3/2/18
 
@@ -8,17 +8,25 @@ Updated: 3/2/18
 
 #include <stdio.h>
 #include <unistd.h> //Sleep
+#include <stdlib.h> //atoi
 
-void main( void ){
+void main( int argc, char *argv[] ){
 
-    int countdown_seconds = 0;
+    int countdown_seconds = 1;
 
-    printf("Enter time to count down in seconds: ");
-    scanf("%i",&countdown_seconds);
+    //Accept command line args
+    if(argc < 1){
+        printf("Enter time to count down in seconds: ");
+        scanf("%i",&countdown_seconds);
+    }else{
+        //Convert string to interger
+        countdown_seconds = atoi(argv[1]);
+    }
 
+    //Actual simple countdown mechanism below
     for(int i = 0; i < countdown_seconds;){
-        countdown_seconds = countdown_seconds - 1;
         printf("Time remaining: %i\n",countdown_seconds);
+        countdown_seconds = countdown_seconds - 1;
         sleep(1);
     }
 
