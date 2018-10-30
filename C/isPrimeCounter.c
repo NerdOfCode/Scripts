20e6a24011af2349d2ca6bb19440d4b32c614dfa
NerdOfCode <admin@nerdofcode.com>
2018-10-30 08:29:18 -0500

    Update isPrimeCounter.c
    :) Updated to present day skills

--- C/isPrimeCounter.c
+++ C/isPrimeCounter.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 
-
-
-int main(){
+int main(void){
 
         int start_numb=2;
         int start_div=2;
         int status;
+        int shorten; 
 
-
-
-        while(1==1){
-                int shorten = start_numb / 2;
+        while(1){
+                shorten = start_numb / 2;
                 if(start_numb % start_div != 0 && start_div <= shorten){
-                        //printf("Number is not prime: %d", start_numb);
                         start_div++;
                 }else if(start_div == shorten + 1){
-
                         printf("Found a prime Number: %d\n", start_numb);
                         start_div = 2;
                         start_numb++;
@@ -25,8 +20,5 @@ int main(){
                         start_div = 2;
                 }
         }
-
-
-
         return 0;
 }
